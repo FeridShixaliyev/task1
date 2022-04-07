4d39f45e70cff04488792ce04f457f2901343e0d
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-07 12:00:50 +0400

    task

---
+++ Book1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp44
+{
+    class Book
+    {
+        public Book()
+        {
+            _id++;
+            Id = _id;
+        }
+
+        private static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public double Price { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id} - Name: {Name} - AuthorName: {AuthorName} - Price: {Price}");
+        }
+    }
+}
