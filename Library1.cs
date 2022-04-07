f75125c851483e58db24fd0f801a492d1ff5ba47
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-07 12:01:11 +0400

    task

---
+++ Library1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp44
+{
+    class Library
+    {
+        public Library()
+        {
+            _id++;
+            Id = _id;
+        }
+        private static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        List<Book> books { get; set; } = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public Book GetBookById(int id)
+        {
+            return books.Find(b => b.Id == id);
+        }
+
+        public void RemoveBook(int id)
+        {
+            books.Remove(books.Find(b => b.Id == id));
+        }
+    }
+}
